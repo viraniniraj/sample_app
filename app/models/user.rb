dc3be11952c139e41c202babe7ef0c01abbccfb1
viraniniraj <viraniniraj@gmail.com>
2013-06-19 18:43:34 +0530

    SignUp finish

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User < ActiveRecord::Base
 
   before_save { |user| user.email = email.downcase }
 
-  validates :name,presence: true, length: {maximum: 8}
+  validates :name,presence: true, length: {maximum: 15}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true,
                     format: { with: VALID_EMAIL_REGEX },
