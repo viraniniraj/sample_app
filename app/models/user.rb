8c0931a9938954aa8e32bda8e65cb93c2a378a65
viraniniraj <viraniniraj@gmail.com>
2013-06-24 15:24:15 +0530

    new temp push

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,17 +2,20 @@
 #
 # Table name: users
 #
-#  id         :integer          not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id              :integer          not null, primary key
+#  name            :string(255)
+#  email           :string(255)
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  password_digest :string(255)
+#  remember_token  :string(255)
+#  admin           :boolean
 #
 
 class User < ActiveRecord::Base
   attr_accessible :email, :name,:password, :password_confirmation
-  has_secure_password
-
+   #attr_reader
+   has_secure_password
   before_save { |user| user.email = email.downcase }
   before_save :create_remember_token
 
